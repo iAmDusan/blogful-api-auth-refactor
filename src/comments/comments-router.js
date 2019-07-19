c9020b2f9cb5056de66f414736cd9d34125bfd28
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-07-19 12:39:36 -0400

    following curriculum

--- src/comments/comments-router.js
+++ src/comments/comments-router.js
@@ -1,24 +1,40 @@
-const express = require('express')
 const path = require('path')
+const express = require('express')
+const xss = require('xss')
 const CommentsService = require('./comments-service')
-const { requireAuth } = require('../middleware/jwt-auth')
 
 const commentsRouter = express.Router()
-const jsonBodyParser = express.json()
+const jsonParser = express.json()
+
+const serializeComment = comment => ({
+  id: comment.id,
+  text: xss(comment.text),
+  date_commented: comment.date_commented,
+  article_id: comment.article_id,
+  user_id: comment.user_id
+})
 
 commentsRouter
   .route('/')
-  .post(requireAuth, jsonBodyParser, (req, res, next) => {
-    const { article_id, text } = req.body
-    const newComment = { article_id, text }
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db')
+    CommentsService.getAllComments(knexInstance)
+      .then(comments => {
+        res.json(comments.map(serializeComment))
+      })
+      .catch(next)
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { text, article_id, user_id, date_commented } = req.body
+    const newComment = { text, article_id, user_id }
 
     for (const [key, value] of Object.entries(newComment))
       if (value == null)
         return res.status(400).json({
-          error: `Missing '${key}' in request body`
+          error: { message: `Missing '${key}' in request body` }
         })
 
-    newComment.user_id = req.user.id
+    newComment.date_commented = date_commented;
 
     CommentsService.insertComment(
       req.app.get('db'),
@@ -28,9 +44,63 @@ commentsRouter
         res
           .status(201)
           .location(path.posix.join(req.originalUrl, `/${comment.id}`))
-          .json(CommentsService.serializeComment(comment))
+          .json(serializeComment(comment))
       })
       .catch(next)
-    })
+  })
+
+commentsRouter
+  .route('/:comment_id')
+  .all((req, res, next) => {
+    CommentsService.getById(
+      req.app.get('db'),
+      req.params.comment_id
+    )
+      .then(comment => {
+        if (!comment) {
+          return res.status(404).json({
+            error: { message: `Comment doesn't exist` }
+          })
+        }
+        res.comment = comment
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeComment(res.comment))
+  })
+  .delete((req, res, next) => {
+    CommentsService.deleteComment(
+      req.app.get('db'),
+      req.params.comment_id
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { text, date_commented } = req.body
+    const commentToUpdate = { text, date_commented }
+
+    const numberOfValues = Object.values(commentToUpdate).filter(Boolean).length
+    if (numberOfValues === 0)
+      return res.status(400).json({
+        error: {
+          message: `Request body must content either 'text' or 'date_commented'`
+        }
+      })
+
+    CommentsService.updateComment(
+      req.app.get('db'),
+      req.params.comment_id,
+      commentToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
 
 module.exports = commentsRouter
--- src/comments/comments-service.js
+++ src/comments/comments-service.js
@@ -1,67 +1,37 @@
-const xss = require('xss')
-
 const CommentsService = {
-  getById(db, id) {
-    return db
-      .from('blogful_comments AS comm')
-      .select(
-        'comm.id',
-        'comm.text',
-        'comm.date_created',
-        'comm.article_id',
-        db.raw(
-          `json_strip_nulls(
-            row_to_json(
-              (SELECT tmp FROM (
-                SELECT
-                  usr.id,
-                  usr.user_name,
-                  usr.full_name,
-                  usr.nickname,
-                  usr.date_created,
-                  usr.date_modified
-              ) tmp)
-            )
-          ) AS "user"`
-        )
-      )
-      .leftJoin(
-        'blogful_users AS usr',
-        'comm.user_id',
-        'usr.id',
-      )
-      .where('comm.id', id)
-      .first()
+  getAllComments(knex) {
+    return knex.select('*').from('blogful_comments')
   },
 
-  insertComment(db, newComment) {
-    return db
+  insertComment(knex, newComment) {
+    return knex
       .insert(newComment)
       .into('blogful_comments')
       .returning('*')
-      .then(([comment]) => comment)
-      .then(comment =>
-        CommentsService.getById(db, comment.id)
-      )
+      .then(rows => {
+        return rows[0]
+      })
   },
 
-  serializeComment(comment) {
-    const { user } = comment
-    return {
-      id: comment.id,
-      text: xss(comment.text),
-      article_id: comment.article_id,
-      date_created: new Date(comment.date_created),
-      user: {
-        id: user.id,
-        user_name: user.user_name,
-        full_name: user.full_name,
-        nickname: user.nickname,
-        date_created: new Date(user.date_created),
-        date_modified: new Date(user.date_modified) || null
-      },
-    }
-  }
+  getById(knex, id) {
+    return knex
+      .from('blogful_comments')
+      .select('*')
+      .where('id', id)
+      .first()
+  },
+
+  deleteComment(knex, id) {
+    return knex('blogful_comments')
+      .where({ id })
+      .delete()
+  },
+
+  updateComment(knex, id, newCommentFields) {
+    return knex('blogful_comments')
+      .where({ id })
+      .update(newCommentFields)
+  },
 }
 
 module.exports = CommentsService
